3c5821ff89096964a9ea8211013276aabe2709bc
Chris <banksjazz87@gmail.com>
2023-06-24 07:22:46 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <?php
+            echo '<h1>This is going to be a php app</h1>'
+        ?>
+    </body>
+</html>
