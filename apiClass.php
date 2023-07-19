34186c00bdedac8f7b34850e7c5f6531bcbc4686
Chris <banksjazz87@gmail.com>
2023-07-19 08:07:52 -0400

    feature: coming up with a method to display all of the comic books pertaining to the character selected

--- apiClass.php
+++ apiClass.php
@@ -6,11 +6,11 @@ class MarvelApi {
     function __construct($id) {
         $this->id = $id;
     }
-    
-    function getAllCharacters($offsetNum) {
+
+    function getResponse($url) {
         include 'private.php';
 
-        curl_setopt($curl, CURLOPT_URL, "https://gateway.marvel.com:443/v1/public/characters?ts=$timestamp&offset=$offsetNum&limit=100&apikey=$publicKey&hash=$md5");
+        curl_setopt($curl, CURLOPT_URL, "$url");
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept:application/json , content-type:application/json'));
@@ -22,21 +22,29 @@ class MarvelApi {
 
         return $result;
     }
+    
+    function getAllCharacters($offsetNum) {
+        include 'private.php';
+
+        $responseData = $this->getResponse("https://gateway.marvel.com:443/v1/public/characters?ts=$timestamp&offset=$offsetNum&limit=100&apikey=$publicKey&hash=$md5");
+
+        return $responseData;
+    }
 
     function getOneCharacter() {
         include 'private.php';
 
-        curl_setopt($curl, CURLOPT_URL, "https://gateway.marvel.com:443/v1/public/characters/$this->id?ts=$timestamp&limit=75&apikey=$publicKey&hash=$md5");
-        curl_setopt($curl, CURLOPT_HEADER, 0);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept:application/json , content-type:application/json'));
+        $responseData = $this->getResponse("https://gateway.marvel.com:443/v1/public/characters/$this->id?ts=$timestamp&limit=75&apikey=$publicKey&hash=$md5");
 
-        $result = curl_exec($curl);
-        curl_close($curl);
+        return $responseData;
+    }
 
-        $result = json_decode($result, true);
+    function getCharacterComics() {
+        include 'private.php';
 
-        return $result;
+        $responseData = $this->getResponse("https://gateway.marvel.com:443/v1/public/characters/$this->id/comics?ts=$timestamp&limit=75&apikey=$publicKey&hash=$md5");
+        
+        return $responseData;
     }
 
 
--- views/character.php
+++ views/character.php
@@ -1,15 +1,3 @@
-<?php
-include '../apiClass.php';
-
-$currentId = $_GET['id'];
-
-$api = new MarvelApi($currentId);
-$characterData = $api->getOneCharacter();
-$characterArray = $characterData['data']['results'][0];
-$name = $characterArray['name'];
-$image = $characterArray['thumbnail']['path'] . "." . $characterArray['thumbnail']['extension'];
-$description = $characterArray['description'];
-?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -19,6 +7,7 @@ $description = $characterArray['description'];
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/assets/index.css">
+        <link rel="stylesheet" href="/assets/character.css">
         <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
@@ -29,6 +18,30 @@ $description = $characterArray['description'];
         <script type="text/javascript" src="/src/main.js"></script>
        
     </head>
+
+    <?php
+        include '../apiClass.php';
+
+        $currentId = $_GET['id'];
+
+        $api = new MarvelApi($currentId);
+        $characterData = $api->getOneCharacter();
+        $characterArray = $characterData['data']['results'][0];
+        $name = $characterArray['name'];
+        $image = $characterArray['thumbnail']['path'] . "." . $characterArray['thumbnail']['extension'];
+        $description = $characterArray['description'];
+
+        $comics = $api->getCharacterComics();
+       
+        //[data][results]: [id]
+
+        foreach ( $comics['data']['results'] as $value ) {
+            $thumb = $value['thumbnail'];
+            var_dump($thumb);
+        }
+        //var_dump($comics['data']['results'][0]['thumbnail']);
+    ?>
+    
     <body>
     <div id="character_page_wrapper">
     <header class="header bg-primary-subtle shadow-sm d-flex flex-row justify-content-center align-items-center">
@@ -36,8 +49,11 @@ $description = $characterArray['description'];
             <h1><?php echo $name ?></h1>
         </div>
     </header>
-    <div>
-        <img src=<?php echo "$image"?> alt="image of <?php $name?>"/>
+    <div class="inner-container d-flex flex-row justify-content-center gap-12" style="margin-top: 2rem; column-gap: 2rem;">
+        <div class="image-container" style="background-image: url('<?php echo $image ?>'">
+
+        </div>
+        <div class="d-flex flex-column">
         <h1><?php echo $name ?></h1>
 
         <?php if ( strlen($description) > 0) {
@@ -45,7 +61,16 @@ $description = $characterArray['description'];
         } else {
             echo "<p>Unfortunately $name has no available description</p>";
         }
+
+        // var_dump($comics);
+        // foreach ( $characterArray['comics']['items'] as $value ) {
+        //     $comicBook = $value['name'];
+        //     echo "<p>$comicBook</p>";
+        // }
         ?>
+
+        
+        </div>
     </div>
     </div>
         
